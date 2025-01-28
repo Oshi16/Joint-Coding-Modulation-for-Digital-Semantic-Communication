2bfd7409a749875760c020e88a6834fef2696bdc
Oshi16 <74727266+Oshi16@users.noreply.github.com>
2025-01-28 13:39:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import torchvision
-from torch.utils.data import DataLoader
-import torch
-import torchvision.transforms as transforms
-from network import JCM
-from train import train
-from evaluation import EVAL
-from utils import init_seeds
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from network import JCM  # TensorFlow version of JCM
+from train import train  # TensorFlow version of train
+from evaluation import EVAL  # TensorFlow version of EVAL
+from utils import init_seeds  # TensorFlow-compatible utilities
 import os
 import argparse
 
@@ -18,54 +17,50 @@ def mischandler(config):
 
 
 def main(config):
-    # initialize random seed
+    # Initialize random seed
     init_seeds()
 
-    # prepare training & test data
-    transform_train = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-
-    train_data = torchvision.datasets.CIFAR10(
-        root=config.dataset_path,
-        train=True,
-        transform=transform_train,
-        download=True
-    )
-    test_data = torchvision.datasets.CIFAR10(
-        root=config.dataset_path,
-        train=False,
-        transform=transform_test,
-        download=True
+    # Prepare training & test data
+    train_datagen = ImageDataGenerator(
+        horizontal_flip=True,
+        rescale=1.0 / 255.0,
+        featurewise_center=True,
+        featurewise_std_normalization=True
     )
 
-    train_loader = DataLoader(dataset=train_data, batch_size=config.batch_size, shuffle=True)
-    test_loader = DataLoader(dataset=test_data, batch_size=config.batch_size, shuffle=True)
+    test_datagen = ImageDataGenerator(
+        rescale=1.0 / 255.0,
+        featurewise_center=True,
+        featurewise_std_normalization=True
+    )
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net = JCM(config, device).to(device)
+    (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+    train_generator = train_datagen.flow(x_train, y_train, batch_size=config.batch_size)
+    test_generator = test_datagen.flow(x_test, y_test, batch_size=config.batch_size)
 
+    # Initialize model and optimizer
+    device = "GPU" if tf.config.list_physical_devices('GPU') else "CPU"
+    print(f"Using device: {device}")
+    net = JCM(config)
+
+    # Load checkpoint if required
     if config.load_checkpoint:
-        model_name = '/{}/'.format(config.mod_method) + \
-                     'CIFAR_SNR{:.3f}_Trans{:d}_{}.pth.tar'.format(
-                         config.snr_train, config.channel_use, config.mod_method)
-        net.load_state_dict(torch.load('./checkpoints' + model_name, map_location=torch.device('cpu')))
+        model_name = f'{config.model_path}/{config.mod_method}_SNR{config.snr_train:.3f}_Trans{config.channel_use}.h5'
+        if os.path.exists(model_name):
+            net.load_weights(model_name)
+            print(f"Loaded checkpoint from {model_name}")
+        else:
+            print(f"No checkpoint found at {model_name}")
 
+    # Train or test
     if config.mode == 'train':
-        print("Training with the modulation scheme {}.".format(config.mod_method))
-        train(config, net, train_loader, test_loader, device)
+        print(f"Training with the modulation scheme {config.mod_method}.")
+        train(config, net, train_generator, test_generator)
 
     elif config.mode == 'test':
         print("Start Testing.")
-        acc, mse, psnr, ssim = EVAL(net, test_loader, device, config)
-        print('acc: {:.3f}, mse: {:3f}, psnr: {:.3f}, ssmi: {:.3f}'.format(acc, mse, psnr, ssim))
+        acc, mse, psnr, ssim = EVAL(net, test_generator, config)
+        print(f'acc: {acc:.3f}, mse: {mse:.3f}, psnr: {psnr:.3f}, ssim: {ssim:.3f}')
 
     else:
         print("Wrong mode input!")
@@ -74,14 +69,14 @@ def main(config):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # model hyper-parameters
+    # Model hyper-parameters
     parser.add_argument('--channel_use', type=int, default=128)
     """Available modulation methods:"""
     """bpsk, 4qam, 16qam, 64qam"""
     parser.add_argument('--mod_method', type=str, default='64qam')
     parser.add_argument('--load_checkpoint', type=int, default=1)
 
-    # training hyper-parameters
+    # Training hyper-parameters
     parser.add_argument('--train_iters', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--lr', type=float, default=1e-4)
@@ -90,7 +85,7 @@ if __name__ == '__main__':
     """The tradeoff hyperparameter lambda between two tasks"""
     parser.add_argument('--tradeoff_lambda', type=float, default=200)
 
-    # misc
+    # Misc
     parser.add_argument('--dataset', type=str, default='cifar')
     parser.add_argument('--mode', type=str, default='train')
     parser.add_argument('--model_path', type=str, default='./models')
